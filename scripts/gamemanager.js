de5051c0585289084b0cbb4a026befe8c171ddd6
bruno koji <bruno.chen.duarte@hotmail.com>
2019-12-25 21:42:15 -0300

    First Commmit Add Base Files

---
+++ scripts/gamemanager.js
@@ -0,0 +1,30 @@
+let gameManager = {
+    setStartGame: function(classType){
+       console.log("Game starts");
+       this.setResetGame(classType);
+       this.setPreFight();
+    },
+    setResetGame: function(classType){
+         switch(classType){
+             case "Warrior":
+                 player = new Player(classType,200,100,200,100,50);
+             break;
+             case "Rogue":
+                 player = new Player(classType,200,100,200,100,50);
+             break;
+             case "Mage":
+                 player = new Player(classType,200,100,100,100,100);
+             break;
+             case "Hunter":
+                 player = new Player(classType,200,100,200,100,50);
+             break;
+         };
+         const interface = document.querySelector('.interface');
+         interface.innerHTML = "<img src=img/avatars-characters/" +
+         classType.toLowerCase() + ".jpg>" + "<h3>" + classType +"</h3>";
+
+    },
+    setPreFight: function(){
+           
+    }
+}
---
+++ scripts/player.js
@@ -0,0 +1,10 @@
+let player;
+
+function Player(classType, health, mana,strenght,agility,speed){
+    this.classType = classType;
+    this.health = health;
+    this.mana = mana;
+    this.strenght = strenght;
+    this.agility = agility;
+    this.speed = speed;
+}
