22ee66bdce03c5faf8ed4fec65a6b7aa1a44764d
bruno koji <bruno.chen.duarte@hotmail.com>
2019-12-26 03:20:33 -0300

    change obj data html code

--- scripts/gamemanager.js
+++ scripts/gamemanager.js
@@ -20,8 +20,20 @@ let gameManager = {
              break;
          };
          const interface = document.querySelector('.interface');
-         interface.innerHTML = "<a><img src=img/avatars-characters/" +
-         classType.toLowerCase() + ".jpg>" + "<h3>" + classType +"</h3></a>";
+        /* interface.innerHTML = "<a><img src=img/avatars-characters/" +
+         classType.toLowerCase() + ".jpg>" + "<div><h3>" + classType +
+         "</h3><p>Health: "+ player.health + "</p><p>Mana: " + player.mana +
+         "</p><p>Strenght: "+ player.strenght + "</p><p>Agility:"+ player.agility + 
+         "</p><p>Speed:"+ player.speed + "</p></div></a>" */
+         function capilalize(word){
+             return word.charAt(0).toUpperCase() + word.slice(1);
+         }
+         let obj_data_html = "";
+         for(let key in player){
+            obj_data_html += `<p>${capilalize(key)}: ${player[key]}</p>`;
+         }
+         interface.innerHTML = `<a><img src="img/avatars-characters/${classType.toLowerCase()}.jpg">
+         <div>${obj_data_html}</div></a>`;
 
     },
     setPreFight: function(){
